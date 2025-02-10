3d3c6f0234c1103a22acec190934470ee939b4f8
owenr-hash <owenr4447@gmail.com>
2025-02-10 15:02:02 -0400

    Add files via upload

---
+++ ejercicio 3 modulo2 .js
@@ -0,0 +1,22 @@
+let suma = 7 + 3;
+console.log("7 + 3 = " + suma);
+
+
+
+
+let resta = 10 - 4;
+console.log("10 - 4 = " + resta);
+
+
+
+let multiplicacion = 6 * 2; 
+console.log( "6 * 2 = " + multiplicacion)
+
+
+
+let divicion = 15 / 3; 
+console.log("15 / 3 = " + divicion)
+
+
+let modulo = 9 % 4 
+console.log("9 % 4 =  " + modulo);
